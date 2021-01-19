e34440070cd099dddef0aea2952400feb993ead1
LaetitiaCR <58287147+LaetitiaCR@users.noreply.github.com>
2021-01-19 23:07:54 +0100

    ajout csss et location javascript

---
+++ WebApplicationFreelancer/wwwroot/js/main.js
@@ -0,0 +1,5 @@
+// JavaScript source code
+//window.location = "https://localhost:44309/Customers/index";
+window.location.href = document.getElementById('lienClients').href;
+
+document.getElementById('insertLargeurEcran').innerHTML = document.body.clientWidth + " px";
